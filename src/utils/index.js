39190a43528fb05d63f3653e7359ad0c9f45c93b
Ryan Krol <ryankr@hotmail.co.uk>
2020-08-02 16:58:10 +0100

    Remove redundant logging from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,7 @@
 export default function baseEndpoint() {
-  window.console.log(process.env.REACT_APP_LOCAL_API)
   if (process.env.REACT_APP_LOCAL_API !== undefined) {
-    window.console.log('using this 1')
-    window.console.log('http://localhost:8003/api')
     return 'http://localhost:8003/api'
   } else {
-    window.console.log('using this 2')
-    window.console.log('http://stockpricedataapi.xyz/api')
     return 'http://stockpricedataapi.xyz/api'
   }
 }
